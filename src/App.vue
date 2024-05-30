5ef8e4cfed91abb449809cb4acc4a017944764da
Anggie Alava <codewithangels@gmail.com>
2024-05-29 19:39:55 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.png"
+      width="125"
+      height="125" />
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/form">Form</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import UserInfo from '../components/UserInfo.vue';
+</script>
+
+<template>
+  <main>
+    <UserInfo />
+  </main>
+</template>
